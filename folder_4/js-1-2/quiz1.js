97baa9f800dfe0587060638e2af3558222c58c43
AmirAli6578 <amirah648887@gmail.com>
2022-01-14 21:08:01 +0330

    night-14/1/2022
    i complete quizes js-1-1 and js-1-2

---
+++ folder_4/js-1-2/quiz1.js
@@ -0,0 +1,6 @@
+let dice1 = Math.floor(Math.random() * 6 + 1)
+let dice2 = Math.floor(Math.random() * 6 + 1)
+
+console.log(`${dice1} and ${dice2} sum dices: ${dice1 + dice2}`);
+
+// 4 and 5 sum dices 9
