a27afbebb93fba79480502988a89f0c23c0448aa
AmirAli6578 <amirah648887@gmail.com>
2022-01-15 11:07:58 +0330

    15/1/2022
    complete booklist

--- folder_2/BookList/script.js
+++ folder_2/BookList/script.js
@@ -3,6 +3,7 @@ const heading = document.querySelectorAll(".title")[1];
 const ul = document.querySelector("ul");
 const inputText = document.querySelector("#add-book input");
 const addBtn = document.querySelector(".button");
+const searchInput = document.querySelector("#search-books input");
 
 const spanDelete = `<span class="delete">حذف</span>`;
 
@@ -21,6 +22,12 @@ addBtn.addEventListener("click", (e) => {
     storeToLocalStorage(inputText.value);
 
     inputText.value = "";
+});
+
+ul.addEventListener("click", (e) => {
+    if (e.target.className === "delete") e.target.parentElement.remove();
+
+    removeFromLocalStorage(e.target.parentElement.children[0].textContent);
 });
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -60,11 +67,40 @@ function storeToLocalStorage(task) {
 
 checkBox.addEventListener("click", () => {
     if (checkBox.checked) {
-        heading.style.display = "none";
         ul.style.display = "none";
     } else {
-        heading.style.display = "block";
         ul.style.display = "block";
+    }
+});
+
+function removeFromLocalStorage(task) {
+    let tasks;
+    if (localStorage.getItem("tasks") === null) {
+        tasks = [];
+    } else {
+        tasks = localStorage.getItem("tasks").split(",");
+    }
+
+    for (let i = 0; i < tasks.length; i++) {
+        if (tasks[i] === task) {
+            tasks.splice(i, 1);
+        }
+    }
+
+    if (tasks.length === 0) {
+        localStorage.clear();
+    } else {
+        localStorage.setItem("tasks", tasks);
+    }
+}
+
+searchInput.addEventListener("keyup", (e) => {
+    for (let book of ul.children) {
+        if (book.firstElementChild.textContent.indexOf(searchInput.value) !== -1) {
+            book.style.display = "block";
+        } else {
+            book.style.display = "none";
+        }
     }
 });
 
