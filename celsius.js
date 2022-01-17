c37cd293752febaa323a940aa78f89471ccdda88
AmirAli6578 <amirah648887@gmail.com>
2022-01-17 23:19:08 +0330

    16/1/2022
    changes

---
+++ celsius.js
@@ -0,0 +1,64 @@
+// celsius to farenhite
+function celsiusToFarenheit(celsius) {
+    const fahrenheit = (celsius * 9) / 5 + 32;
+    return `${celsius}°C is ${fahrenheit}°F`;
+}
+console.log(celsiusToFarenheit(32));
+// 32°C is 89.6°F
+
+// with for loop
+function finMax(arr) {
+    const newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        const curr = arr[i];
+        if (curr % 2 === 0) {
+            newArr.push(curr);
+        }
+    }
+    return Math.max(...newArr);
+}
+
+console.log(finMax([20, 40, 200])); // 200
+console.log(finMax([20, 40, 200, 301])); // 200
+
+// with for loop capital string
+function trasforming(str) {
+    let result = "";
+    for (let i = 0; i < str.length; i++) {
+        const curr = str[i];
+        if (curr !== curr.toLowerCase()) {
+            result += curr.toLowerCase();
+        } else {
+            result += curr.toUpperCase();
+        }
+    }
+    return result;
+}
+
+console.log(trasforming("amirali"));
+
+function triple(n1, n2) {
+    if (n1 === n2) {
+        return (n1 + n2) * 3;
+    } else {
+        return n1 + n2;
+    }
+}
+console.log(triple(10, 12));
+
+// multipleOrNot
+function multipleOrNot(n1, n2) {
+    if (n1 < 0 || n2 < 0) {
+        return false
+    }
+
+    function isMultiple(num) {
+        return num % 7 === 0 || num % 11 === 0;
+    }
+
+    const isN1 = isMultiple(n1)
+    const isN2 = isMultiple(n2)
+
+    return Boolean(isN1 ^ isN2)
+}
+console.log(multipleOrNot(10, 20)); // false
