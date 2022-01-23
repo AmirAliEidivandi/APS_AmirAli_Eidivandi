72f30db0f43f3c5b545d628436441bdd2f089085
AmirAli6578 <amirah648887@gmail.com>
2022-01-23 20:38:37 +0330

    23/1/2022
    change files

--- folder_11/script.js
+++ folder_11/script.js
@@ -1,20 +1,24 @@
-// const num = 5;
-// const num2 = -4;
-// const num3 = 'hello'
+const num = 5;
+const num2 = -4;
+const num3 = 'hello'
 
-// let res = Math.sqrt(num);
-// let res2 = Math.sqrt(num2);
-// let res3 = Math.sqrt(num3);
-// console.log(`the square root of ${num} is ${res}`);
-// console.log(`the square root of ${num2} is ${res2}`);
-// console.log(`the square root of ${num3} is ${res3}`);
+let res = Math.sqrt(num);
+let res2 = Math.sqrt(num2);
+let res3 = Math.sqrt(num3);
+console.log(`the square root of ${num} is ${res}`);
+console.log(`the square root of ${num2} is ${res2}`);
+console.log(`the square root of ${num3} is ${res3}`);
 
-const side = 23;
-const side2 = 543;
-const side3 = 872;
+// const side = 23;
+// const side2 = 543;
+// const side3 = 872;
 
-const s = (side + side2 + side3) / 2;
+// const s = (side + side2 + side3) / 2;
 
-const areaValue = Math.sqrt(s * (s - side) * (s - side2) * (s - side3));
+// const areaValue = Math.sqrt(s * (s - side) * (s - side2) * (s - side3));
 
-console.log(`the area of the triangle is ${areaValue}`);
+// console.log(`the area of the triangle is ${areaValue}`);
+
+// const hello = "amirlai";
+
+// const ali = "ahmad";
