cafbd2f75a39a3aaecec02f77703cf7974e43b61
AmirAli6578 <amirah648887@gmail.com>
2022-02-28 21:18:06 +0330

    prettier

--- oop/setter-getter.js
+++ oop/setter-getter.js
@@ -1,16 +1,16 @@
 const newObj = {
-    name: 'amirali',
-    family: 'eidivandi',
+    name: "amirali",
+    family: "eidivandi",
     get age() {
         return this.userAge;
     },
     set age(uAge) {
         if (uAge < 20) {
-            throw new Error('this is a access')
+            throw new Error("this is a access");
         }
         this.userAge = uAge;
-    }
-}
+    },
+};
 
 newObj.age = 30;
 
