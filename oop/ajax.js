5ace79fc3657406415c32ba0c8d4f77bbad4b2e2
AmirAli6578 <amirah648887@gmail.com>
2022-03-02 00:36:11 +0330

    fetch api

--- oop/ajax.js
+++ oop/ajax.js
@@ -1,49 +1,66 @@
 const getBtn = document.querySelector(".get-data");
 const postBtn = document.querySelector(".post-data");
 
+// data with ajax
+// const sendHttpRequest = (method, url, data) => {
+//     const response = new Promise((resolve, reject) => {
+//         const xhr = new XMLHttpRequest();
+//         xhr.open(method, url);
+//         xhr.responseType = "json";
+//         if (data) {
+//             xhr.setRequestHeader("Content-Type", "application/json");
+//         }
+//         xhr.onload = () => {
+//             resolve(xhr.response);
+//         };
+//         xhr.onerror = () => {
+//             reject("not found...");
+//         };
+//         xhr.send(JSON.stringify(data));
+//     });
+//     return response;
+// };
+
+// function getData() {
+//     sendHttpRequest("GET", "https://jsonplaceholder.typicode.com/todos/")
+//         .then((data) => {
+//             console.log(data);
+//         })
+//         .catch((err) => {
+//             console.log(err);
+//         });
+// }
+
+// function postData() {
+//     sendHttpRequest("POST", "https://jsonplaceholder.typicode.com/todos/", {
+//         userId: 2,
+//         id: 2,
+//         title: "Post Title",
+//         body: "Post Body",
+//     })
+//         .then((data) => {
+//             console.log(data);
+//         })
+//         .catch((err) => {
+//             console.log(err);
+//         });
+// }
+
+// getBtn.addEventListener("click", getData);
+// postBtn.addEventListener("click", postData);
+
+// data with fetch api
 const sendHttpRequest = (method, url, data) => {
-    const response = new Promise((resolve, reject) => {
-        const xhr = new XMLHttpRequest();
-        xhr.open(method, url);
-        xhr.responseType = "json";
-        if (data) {
-            xhr.setRequestHeader("Content-Type", "application/json");
-        }
-        xhr.onload = () => {
-            resolve(xhr.response);
-        };
-        xhr.onerror = () => {
-            reject("not found...");
-        };
-        xhr.send(JSON.stringify(data));
+    return fetch(url).then((res) => {
+        return res.json();
     });
-    return response;
 };
-
 function getData() {
-    sendHttpRequest("GET", "https://jsonplaceholder.typicode.com/todos/")
-        .then((data) => {
-            console.log(data);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+    sendHttpRequest("GET", "https://jsonplaceholder.typicode.com/todos/").then((data) => {
+        console.log(data);
+    });
 }
-
-function postData() {
-    sendHttpRequest("POST", "https://jsonplaceholder.typicode.com/todos/", {
-        userId: 2,
-        id: 2,
-        title: "Post Title",
-        body: "Post Body",
-    })
-        .then((data) => {
-            console.log(data);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-}
+function postData() {}
 
 getBtn.addEventListener("click", getData);
 postBtn.addEventListener("click", postData);
