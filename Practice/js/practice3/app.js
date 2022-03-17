6ba4a3773dd31d8143132bf5cc8466bea10674ba
AmirAli6578 <amirah648887@gmail.com>
2022-03-17 23:26:45 +0330

    contructor function

--- Practice/js/practice3/app.js
+++ Practice/js/practice3/app.js
@@ -12,18 +12,37 @@
 
 // person.great();
 
-const person = {
-    firstName: "amirali",
-    lastName: "eidivandi",
-    greet: function () {
-        console.log("hello world amirali eidivandi");
-    },
-    // getFullName() {
-    //     return `name: ${this.firstName} - lastName: ${this.lastName}`;
-    // },
-    getFullName: function () {
-        return `name: ${this.firstName} - lastName: ${this.lastName}`;
-    },
-};
+// const person = {
+//     firstName: "amirali",
+//     lastName: "eidivandi",
+//     greet: function () {
+//         console.log("hello world amirali eidivandi");
+//     },
+//     // getFullName() {
+//     //     return `name: ${this.firstName} - lastName: ${this.lastName}`;
+//     // },
+//     getFullName: function () {
+//         return `name: ${this.firstName} - lastName: ${this.lastName}`;
+//     },
+// };
 
-console.log(person.getFullName());
+// console.log(person.getFullName());
+
+// constructor funciton
+
+function Person(fName, lName) {
+    if (!new.target) {
+        throw Error('Cannot be called without the new keyword')
+    }
+
+    this.fName = fName;
+    this.lName = lName;
+
+    this.getFullName = function () {
+        const { fName, lName } = this;
+        return `firstName: ${fName} - lastName: ${lName}`;
+    };
+}
+
+let person1 = Person("amirali", "eidivandi");
+console.log(person1.getFullName());
