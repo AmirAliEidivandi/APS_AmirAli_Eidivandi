8d117984b977ae4bed0050e41e99674d94eef8b5
AmirAli6578 <amirah648887@gmail.com>
2022-04-08 14:20:55 +0430

    complete project

---
+++ digital-clock-2/script.js
@@ -0,0 +1,15 @@
+const hr = document.querySelector("#hr");
+const mn = document.querySelector("#mn");
+const sc = document.querySelector("#sc");
+
+let day = new Date();
+
+setInterval(() => {
+    let hh = day.getHours() * 30;
+    let mm = day.getMinutes() * 6;
+    let ss = day.getSeconds() * 6;
+
+    hr.style.transform = `rotateZ(${hh + mm / 12}deg)`;
+    mn.style.transform = `rotateZ(${mm}deg)`;
+    sc.style.transform = `rotateZ(${ss}deg)`;
+});
